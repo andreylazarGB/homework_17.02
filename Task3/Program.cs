42301d60b9abe6fc94cc401a7ef850e2ca6647d1
andrey lazar <andreylazarmr@gmail.com>
2023-02-22 12:02:39 +0300

    Task1

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Clear();
+
+Console.WriteLine("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int GetCubeNum(int num)
+{
+    int result = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        result = Convert.ToInt32(Math.Pow(i, 3));    
+        Console.WriteLine($"Куб числа  {i} : {result}");     
+    }
+    return result;
+}
+
+GetCubeNum(a);
+
